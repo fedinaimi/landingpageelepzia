4f2114ed8f8406b4bd72ee8a9baaa47ef9f11ce7
fedinaimi <fedi.naimi@esprit.tn>
2025-01-03 00:36:33 +0100

    fix

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -59,6 +59,8 @@ function NavLinks() {
     const links = [
         { name: 'Home', href: 'home' },
         { name: 'Episafe', href: 'episafe' },
+        { name: 'Milestones', href: 'roadmap' },
+
         { name: 'Team', href: 'team' },
         { name: 'Experts', href: 'experts' },
         { name: 'Contact', href: 'contact' }
@@ -108,6 +110,8 @@ function MobileDrawer({
     const links = [
         { name: 'Home', href: 'home' },
         { name: 'Episafe', href: 'episafe' },
+        { name: 'Milestones', href: 'roadmap' },
+
         { name: 'Team', href: 'team' },
         { name: 'Experts', href: 'experts' },
         { name: 'Contact', href: 'contact' }
